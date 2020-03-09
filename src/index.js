08e914146fbf8a5adf6321ba9d85130c66c680f2
Stephen Esser <Stephen.Esser@Cerner.com>
2020-03-09 18:38:46 -0500

    Update polyfills to be included in the entry point file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// Polyfills for IE. https://reactjs.org/docs/javascript-environment-requirements.html
+import 'core-js/es/map';
+import 'core-js/es/set';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Example from './Example';
